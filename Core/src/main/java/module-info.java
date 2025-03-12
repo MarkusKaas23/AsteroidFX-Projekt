fffa3f534d1ae64725e1b8b130dfa19ad176b2d6
MarkusKaas23 <143800614+MarkusKaas23@users.noreply.github.com>
2025-03-12 10:05:13 +0100

    Add files via upload

---
+++ Core/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module Core {
+    exports dk.sdu.cbse;  // Export the package containing dk.sdu.cbse.App
+    // Add any required dependencies below if necessary
+    // requires another.module; // Example: if you need dependencies
+
+}
